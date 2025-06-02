0b19f111f65dc471eb3f00ae2a750714b5517c2d
Oget Charlene <charlene.oget@epitech.eu>
2025-06-03 00:11:57 +0200

    Adding route not tested on postman yet

--- tasks-manager/routes/web.php
+++ tasks-manager/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return redirect()->route('tasks.index');
+// });
+
+// Route::resource('tasks', TaskController::class);
+
+Route::get('/test-api', function() {
+    return response()->json(['status'=> 'ça marche ouuuuuuuuu']);
 });
