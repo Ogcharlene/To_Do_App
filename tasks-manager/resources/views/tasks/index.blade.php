e85fe1f27c1c4a66440501a6beb71056d8189d04
Oget Charlene <charlene.oget@epitech.eu>
2025-06-04 21:00:02 +0200

    adding tasks view

--- tasks-manager/resources/views/tasks/index.blade.php
+++ tasks-manager/resources/views/tasks/index.blade.php
@@ -9,20 +9,44 @@
 <body class="flex items-center flex-col p-4 bg-gray-200 min-h-screen">
     <div class="w-full max-w-xl bg-white shadow-sm rounded-2xl p-8 border border-[#e0e0e0]">
         <h1 class="text-3xl font-semibold mb-6">My To Do App</h1>
-        
-    
 
-        <div class="w-full max-w-xl bg-white shadow-sm rounded-2xl p-8 border border-[#e0e0e0] mt-10">
-            <ul class="list-none space-y-5">
-                <li>
-                <input type="checkbox"> <span>Faire projet tech dsi casino</span>
-                </li>
-                <li>
-                <input type="checkbox"> <span class="">faire un mini configurateur 3D</span>
-                </li>
+        <form action="{{ route('tasks.store') }}" method="POST" class="">
+           @csrf <!-- envoie un token pour chaque soumission de formulaire -> Protection (pour ça que les test postman autre que get ne marchaient pas) -->
+           <input name="title" type="text" placeholder="Titre....." class="flex-1 border rounded p-2" required>
+           <button type="submit" class="bg-black text-white px-4 py-2 rounded">Ajouter</button>
+        </form>
+
+        <!-- Message de confirmation (success key) -->
+        @if(session('success'))
+            <div class="mb-4">
+                {{session('success')}}
+            </div>
+        @endif
+
+            <ul class="list-none space-y-5 mt-10">
+                @forelse($tasks as $task)
+                    <li class="flex justify-between items-center bg-gray-100 px-4 py-3 rounded">
+                        <div class="flex items-center gap-2">
+                            <input type="checkbox" {{$task->is_done ? 'checked' : ''}}>
+                            <span class="{{$task->is_done ? 'line-through text-gray-500' : ''}}">
+                                {{$task->title}}
+                            </span>
+                        </div>
+                        <div>
+                            <a href="{{route('tasks.edit', $task->id)}}" class="text-blue-600 hover:underline text-sm">Modifier</a>
+                            <form action="{{route('tasks.destroy', $task->id)}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="text-red-500 hover:underline text-sm">Supprimer</button>
+                            </form>
+                        </div>
+                    </li>
+                @empty
+                    <li class="text-gray-500 text-center">Aucune tâche disponible</li>
+                @endforelse
             </ul>
-        </div>
     </div>
 </body>
 
 </html>
+
